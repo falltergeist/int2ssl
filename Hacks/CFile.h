c16c81a927e1767111ed78de2f90d54df7e1e053
alexeevdv <mail@alexeevdv.ru>
2015-02-15 21:33:27 +0300

    CArray removed

--- Hacks/CFile.h
+++ Hacks/CFile.h
@@ -1,8 +1,8 @@
 /**
  *
  * Copyright (c) 2005-2009 Anchorite (TeamX), <anchorite2001@yandex.ru>
- * Copyright (c) 20014-2015 Nirran, phobos2077
- * Copyright (c) 20015 alexeevdv <mail@alexeevdv.ru>
+ * Copyright (c) 2014-2015 Nirran, phobos2077
+ * Copyright (c) 2015 alexeevdv <mail@alexeevdv.ru>
  * Distributed under the GNU GPL v3. For full terms see the file license.txt
  *
  */
